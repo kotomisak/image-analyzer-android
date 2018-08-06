643bdfa5c6f0d489ff8894ee55e293cd2cab37be
Michal <jenicek.michal@gmail.com>
2018-08-06 09:56:34 +0200

    Fix not-generated Args

--- module-feature-1/src/main/java/com/example/android/feature1/FlowStepFragment.kt
+++ module-feature-1/src/main/java/com/example/android/feature1/FlowStepFragment.kt
@@ -16,37 +16,43 @@
 
 package com.example.android.feature1
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 /**
  * Presents how multiple steps flow could be implemented.
  */
 class FlowStepFragment : Fragment() {
 
-//	override fun onCreateView(
-//		inflater: LayoutInflater, container: ViewGroup?,
-//		savedInstanceState: Bundle?
-//	): View? {
-//		setHasOptionsMenu(true)
-//
-////		val step = arguments?.getInt("step")
-//
-//		val step = arguments?.let {
-//			val safeArgs = FlowStepFragmentArgs.fromBundle(it)
-//			safeArgs.step
-//		}
-//
-//		return when (step) {
-//			2 -> inflater.inflate(R.layout.flow_step_two_fragment, container, false)
-//			else -> inflater.inflate(R.layout.flow_step_one_fragment, container, false)
-//		}
-//	}
-//
-//	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//		super.onViewCreated(view, savedInstanceState)
-//
-//		view.findViewById<View>(R.id.next_button).setOnClickListener(
-//			Navigation.createNavigateOnClickListener(R.id.next_action)
-//		)
-//	}
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		setHasOptionsMenu(true)
+
+		//val step = arguments?.getInt("step")
+
+		val step = arguments?.let {
+
+			val safeArgs = FlowStepFragmentArgs.fromBundle(it)
+			safeArgs.step
+		}
+
+		return when (step) {
+			2 -> inflater.inflate(R.layout.flow_step_two_fragment, container, false)
+			else -> inflater.inflate(R.layout.flow_step_one_fragment, container, false)
+		}
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		view.findViewById<View>(R.id.next_button).setOnClickListener(
+			Navigation.createNavigateOnClickListener(R.id.next_action)
+		)
+	}
 }
